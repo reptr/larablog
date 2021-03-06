6b84560f598b4176978403d7bc202d161a6036e4
reptr <restu.hanputraa@gmail.com>
2021-03-06 18:42:15 +0700

    add category check input & edit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,4 +6,12 @@
             <h3 class="text-white font-weight-bold">Admin Dashboard</h3>
         </div>
     </div>
+
+    <div class="container">
+        <div class="col-md-12">
+            <a href="{{ route('blogs.create') }}" class="btn btn-lg btn-primary btn-margin-right">Create Blog</a>
+            <a href="{{ route('blogs.trash') }}" class="btn btn-lg btn-danger btn-margin-right">Trashed Blog</a>
+            <a href="{{ route('categories.create') }}" class="btn btn-lg btn-success btn-margin-right">Create Categories</a>
+        </div>
+    </div>
 @endsection
