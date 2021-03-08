8427bc6cfa8e2646dda1ee7f8571e828dfcc4ca2
restuhanputra <restu.hanputraa@gmail.com>
2021-03-08 08:50:24 +0700

    add partials, meta_static & meta_description

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
+
 @section('content')
-    <div class="container">
+
+@include('partials.meta_static')
+
+<div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 {{-- <div class="container">
